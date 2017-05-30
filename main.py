8791c1b620f479dacc4dfe6d97aafd44693cdeef
Mark B <mpboddie@gmail.com>
2017-05-29 21:42:54 -0400

    Cleaned up some old files

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import json
+import searchItems
 
 with open('config.json', 'r') as json_data:
     config = json.load(json_data)
 
-print(config["siteSettings"]["address"])
+#print(config["siteSettings"]["address"])
 
 with open('search.json', 'r') as json_data:
     search = json.load(json_data)
 
-print(search[0]["title"])
+#print(search[0]["title"])
+
+for show in search:
+    tvshow = searchItems.Show(show["title"], show["season"], show["format"], show["resolution"])
+    print(tvshow.searchSite())
