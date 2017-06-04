f26ad1b1d1d7477bb26edcc37e2f4dd8d3e9b3d0
Mark B <mpboddie@gmail.com>
2017-06-03 21:53:36 -0400

    Login has been added

--- searchItems.py
+++ searchItems.py
@@ -1,9 +1,43 @@
 import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+def check_exists_by_id(browser,id):
+    try:
+        browser.find_element_by_id(id)
+    except NoSuchElementException:
+        return False
+    return True
 
 class SearchItems:
 
     with open('config.json', 'r') as json_data:
-        site = json.load(json_data)["siteSettings"]["address"]
+        config = json.load(json_data)
+
+    site = config["siteSettings"]["address"]
+    username = config["siteSettings"]["username"]
+    password = config["siteSettings"]["password"]
+
+    browser = webdriver.PhantomJS()
+    browser.get(site)
+
+    def login(self):
+        print("Logging in!")
+        try:
+            userField = self.browser.find_element_by_name("username")
+            passField = self.browser.find_element_by_name("password")
+            submit = self.browser.find_element_by_class_name("button")
+        except NoSuchElementException:
+            return False
+        userField.clear()
+        userField.send_keys(self.username)
+        passField.clear()
+        passField.send_keys(self.password)
+        submit.click()
+
+        return check_exists_by_id(self.browser, "iptStart")
+
 
 class Show(SearchItems):
 
@@ -25,4 +59,13 @@ class Show(SearchItems):
             self.site = site
 
     def searchSite(self):
+
+        if check_exists_by_id(self.browser, "login"):
+            if not self.login():
+                return "ERROR: Problem logging in"
+
+        #login = self.browser.find_element_by_id('login')
+        #print(self.browser.page_source)
+        self.browser.save_screenshot('screenie.png')
+        #self.browser.quit()
         return "Searching {} for an episode of {} from season {}.".format(self.site, self.title, self.season)
