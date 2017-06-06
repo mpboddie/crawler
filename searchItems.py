9c16dddf7fca185704d7cbc041c686cef58e1612
Mark B <mpboddie@gmail.com>
2017-06-05 22:48:58 -0400

    It is now interacting with the checkboxes and getting search results for the title name, not doing anything with the resuts yet

--- searchItems.py
+++ searchItems.py
@@ -38,8 +38,14 @@ class SearchItems:
 
         return check_exists_by_id(self.browser, "iptStart")
 
+    def quit(self):
+        self.browser.quit()
+        return True
 
 class Show(SearchItems):
+
+    tvFormat = {'TV/Web-DL': 13, 'TV/x264': 14, 'TV/x265': 15, 'TV/Xvid': 16}
+    resFormat = {'720p': 7, '1080p': 8, '2160p': 9}
 
     def __init__(self, title, season, vidFormat, resolution, site=None):
         self.title = title
@@ -60,12 +66,25 @@ class Show(SearchItems):
 
     def searchSite(self):
 
+        self.browser.get(self.site + 't')
+
         if check_exists_by_id(self.browser, "login"):
             if not self.login():
                 return "ERROR: Problem logging in"
 
-        #login = self.browser.find_element_by_id('login')
-        #print(self.browser.page_source)
+        print(check_exists_by_id(self.browser, "iptStart"))
+        for resPref in self.resolution:
+            for vidPref in self.vidFormat:
+                self.browser.execute_script("$('#catters :input[type=\"checkbox\"]').prop('checked', false);");
+                self.browser.execute_script('$(\'#catters > table > tbody > tr > td:nth-child(2) > label:nth-child(' + str(self.tvFormat[vidPref]) + ') > input[type="checkbox"]\').prop(\'checked\', true);')
+                self.browser.execute_script('$(\'#catters > table > tbody > tr > td:nth-child(6) > label:nth-child(' + str(self.resFormat[resPref]) + ') > input[type="checkbox"]\').prop(\'checked\', true);')
+                qInput = self.browser.find_element_by_name('q')
+                qInput.clear()
+                qInput.send_keys(self.title)
+                qButton = self.browser.find_element_by_xpath('//*[@id="Search"]/tbody/tr/td/input[2]')
+                qButton.click()
+                print("Searching for an episode of {} from season {} in {} format with {} resolution.".format(self.title, self.season, vidPref, resPref))
+
         self.browser.save_screenshot('screenie.png')
-        #self.browser.quit()
+
         return "Searching {} for an episode of {} from season {}.".format(self.site, self.title, self.season)
